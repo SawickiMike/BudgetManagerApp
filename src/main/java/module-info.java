cff9e26543899b31f5b7a7e9e6d37750cfd94e09
SawickiMike <120393520+SawickiMike@users.noreply.github.com>
2023-12-19 12:38:42 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.sawickimike.budgetmanagerapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.sawickimike.budgetmanagerapp to javafx.fxml;
+    exports com.sawickimike.budgetmanagerapp;
+}
